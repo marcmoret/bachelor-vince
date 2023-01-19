297eacbd9d36654994d947097e90a6cea7590447
marcmoret <marco.moretto@me.com>
2023-01-19 14:27:21 -0500

    first few components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForumComponent } from './forum/forum.component';
+import { HomeComponent } from './home/home.component';
+import { InformationComponent } from './information/information.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'forum', component: ForumComponent },
+  { path: 'info', component: InformationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ForumComponent } from './forum/forum.component';
+import { InformationComponent } from './information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ForumComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
